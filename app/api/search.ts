d25a4174a410d16f46932f8b80c619201e870f87
Hassam Uddin <hassamuddin20@gmail.com>
2022-11-14 01:26:46 -0600

    migrate to directly use gcloud function. Pass it in as an environment variable.

--- app/api/search.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-  const function_url = 'https://get-apartments-wmutchbu7q-uc.a.run.app'
-  const url = `${function_url}?${req.query}`
-  const data = await fetch(url)
-  const json = await data.json()
-  res.json(json)
-}
--- app/env.d.ts
+++ app/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_FUNCTION_URL: string
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
