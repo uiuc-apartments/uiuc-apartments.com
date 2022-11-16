eaf91e12ae08d22572ba0b3f78d64d55cfb8e506
Richard Liu <richy.liu.2002@gmail.com>
2022-11-16 15:22:51 -0600

    rename cloud function
    rename hello_get to build_apartments

--- function_build/deploy.sh
+++ function_build/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-gcloud functions deploy python-http-function \
---gen2 \
+gcloud functions deploy build-apartments \
+    --gen2 \
     --runtime=python310 \
     --region=us-central1 \
     --source=. \
-    --entry-point=hello_get \
+    --entry-point=build_apartments \
     --trigger-http \
     --allow-unauthenticated \
     --project=champaign-apartment-aggregator
---
+++ function_build/trigger.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+gcloud functions call build-apartments --gen2 --region=us-central1
