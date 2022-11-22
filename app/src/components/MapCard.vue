c9136d568b404dc125226122c03233d09840bda8
Hassam Uddin <hassamuddin20@gmail.com>
2022-11-22 13:41:06 -0600

    speed up marker drawing and combine duplicated markers

--- app/src/components/MapCard.vue
+++ app/src/components/MapCard.vue
@@ -3,7 +3,9 @@ import leaflet from 'leaflet'
 import type { Apartment } from '../types'
 
 let map: leaflet.Map
+let renderer: leaflet.Canvas
 let markersLayer: leaflet.LayerGroup = leaflet.layerGroup()
+let placed = new Map<string, leaflet.CircleMarker>()
 
 export default {
   props: {
@@ -18,6 +20,7 @@ export default {
   watch: {
     apartments(newVal: Apartment[]) {
       markersLayer.clearLayers()
+      placed.clear()
       // remove old layer if necessary
       if (map.hasLayer(markersLayer)) {
         map.removeLayer(markersLayer)
@@ -27,14 +30,24 @@ export default {
       // create new layer
       for (const apartment of newVal) {
         // TODO: the lat long was flipped in the DB, correcting it here
-        const location = [apartment.longitude, apartment.latitude]
+        const location = leaflet.latLng(apartment.longitude, apartment.latitude)
         const description = `${apartment.address} - ${apartment.bedrooms} BR / ${apartment.bathrooms} BA - $${apartment.rent}`
-        const marker = leaflet
-          .marker(location)
-          .bindPopup(description)
-        markersLayer.addLayer(marker)
+        var marker: leaflet.CircleMarker | undefined;
+        marker = placed.get(apartment.address)
+        if(marker !== undefined) {
+          var currentPopup = marker.getPopup()?.getContent()
+          marker.setPopupContent(currentPopup + "<br>" + description)
+        } else {
+          marker = leaflet
+            .circleMarker(location, {
+              renderer: renderer
+            })
+            .bindPopup(description)
+            markersLayer.addLayer(marker)
+        }
+        placed.set(apartment.address, marker);
       }
-      console.log('markersLayer', markersLayer)
+      // console.log('markersLayer', markersLayer)
 
       // add new layer
       markersLayer.addTo(map)
@@ -43,6 +56,7 @@ export default {
   },
   mounted() {
     // init map
+    renderer = leaflet.canvas({ padding: 0.5 });
     map = leaflet
       .map('map', {
         zoomControl: true,
