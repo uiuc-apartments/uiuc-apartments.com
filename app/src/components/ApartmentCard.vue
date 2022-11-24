94e3c8e6d62254d97581ada06c5652a9e08bda30
Richard Liu <richy.liu.2002@gmail.com>
2022-11-24 15:37:19 -0600

    minor ui tweaks
    - update title
    - show per person price rounded to 2 decimal places
    - add padding around agency filters because the checkboxes were hard to
      click

--- app/src/components/ApartmentCard.vue
+++ app/src/components/ApartmentCard.vue
@@ -8,8 +8,15 @@ export default {
     },
   },
   computed: {
-    pricePerPerson(): number {
-      return this.source.rent / Math.max(1, this.source.bedrooms)
+    pricePerPerson(): string {
+      // show the price per person rounded to 2 decimal places only if it is not an integer
+      let perPerson = this.source.rent / Math.max(1, this.source.bedrooms)
+      let rounded = Math.round(perPerson * 100) / 100
+      let tolerance = 0.001
+      if (rounded - Math.floor(rounded) > tolerance) {
+        return rounded.toFixed(2)
+      }
+      return rounded.toString()
     },
   },
 }
@@ -21,12 +28,18 @@ export default {
     class="my-3 relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
   >
     <div class="min-w-0 flex-1">
-      <a :href="source.link" target="_blank" rel="noreferrer noopener" class="focus:outline-none">
+      <a
+        :href="source.link"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="focus:outline-none"
+      >
         <p class="text-sm font-medium text-gray-900">{{ source.address }}</p>
         <div class="truncate text-sm text-gray-500">
           <span class="font-bold text-gray-900">
-            <font-awesome-icon icon="dollar-sign" /> {{ source.rent }} / <font-awesome-icon icon="dollar-sign" />  {{ pricePerPerson }}
-              
+            <font-awesome-icon icon="dollar-sign" />
+            {{ source.rent }} (<font-awesome-icon icon="dollar-sign" />
+            {{ pricePerPerson }} / person)
           </span>
           <div v-if="source.is_studio">
             <font-awesome-icon icon="bed" /> Studio
